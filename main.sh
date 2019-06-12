942bd6edaea6a0da13350417de473f8e7f72bf51
Jeremy Greer <jex.grizzle@gmail.com>
2019-06-12 14:49:07 -0700

    Add support for remote templates

--- main.sh
+++ main.sh
@@ -12,7 +12,7 @@ if [ -z $DESTINATION ]; then
 fi
 
 if [ $1 = "--version" ]; then
-  echo "1.2.0"
+  echo "1.3.0"
   exit 0
 fi
 
@@ -29,49 +29,58 @@ if [ -z $TEMPLATE ]; then
   echo "You must specify the template."
 
   # list available
-  ls $TEMPLATE_DIR
+  ls  $TEMPLATE_DIR
   exit 1
 fi
 
+
 # Can we find this template?
 TEMPLATE_PATH=$TEMPLATE_DIR$TEMPLATE
-if [ -d $TEMPLATE_PATH ]; then
-
-  # TODO: Separate destination path and basename.
-  if [ -z $DESTINATION ]; then
-    echo "What is the name of the new project?"
-    exit 1
-
-  else
-    # echo "  called $DESTINATION."
-
-    if [ -d $DESTINATION ]; then
-      echo "$DESTINATION already exists."
-      exit 1
-    else
-
-      git clone $TEMPLATE_PATH $DESTINATION &>/dev/null
-      CLONE_RESULT=$?
-      if [ $CLONE_RESULT -ne 0 ]; then
-        echo "The template could not be cloned."
-        exit 1
-
-      else
-
-        cd $DESTINATION
-        # Replace the placeholders with the project name.
-        # TODO: Note dependency on Ag.
-        ag PROJECT_NAME -l | xargs sed -e "s/PROJECT_NAME/$DESTINATION/g" -i ''
-
-        # Remove Git evidence.
-        rm -rf .git
-
-        # echo "👍 Party on."
-      fi
-    fi
+if [ -d $TEMPLATE_PATH ] || [ -h $TEMPLATE_PATH ]; then
+  if [ -h $TEMPLATE_PATH ]; then
+    # linked to remote repo
+    TEMPLATE_PATH=$(readlink $TEMPLATE_PATH)
   fi
-
 else
   echo "$TEMPLATE_PATH does not exist."
+
+  # TODO: make function
+  # list available
+  ls  $TEMPLATE_DIR
   exit 1
+fi
+
+
+# TODO: Separate destination path and basename.
+if [ -z $DESTINATION ]; then
+  echo "What is the name of the new project?"
+  exit 1
+
+else
+  # echo "  called $DESTINATION."
+
+  if [ -d $DESTINATION ]; then
+    echo "$DESTINATION already exists."
+    exit 1
+  else
+
+    git clone $TEMPLATE_PATH $DESTINATION &>/dev/null
+    CLONE_RESULT=$?
+    if [ $CLONE_RESULT -ne 0 ]; then
+      echo "The template could not be cloned."
+      exit 1
+
+    else
+
+      cd $DESTINATION
+      # Replace the placeholders with the project name.
+      # TODO: Note dependency on Ag.
+      ag PROJECT_NAME -l | xargs sed -e "s/PROJECT_NAME/$DESTINATION/g" -i ''
+
+      # Remove Git evidence.
+      rm -rf .git
+
+      # echo "👍 Party on."
+    fi
+  fi
 fi
